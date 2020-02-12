f39b6b0a5674945babebc75509f06112960dbd31
David Komer <david.komer@gmail.com>
2020-02-12 15:43:27 +0200

    header lang

--- src/pages/common/header.ts
+++ src/pages/common/header.ts
@@ -1,10 +1,16 @@
 import {html} from "lit-html";
 import "./header.css"
 import { CdnPath } from "@utils/path";
+import {set_language, get_language} from "@components/language-selector/language-selector-state";
+export const header = () => {
+    const lang = get_language();
 
-export const header = () => html`
-    <header class="main-header">
-        <img class="logo" src=${CdnPath.common("tj-logo-header.png")} />
-        <div class="text">Learn Words!</div>
-    </header>
-`;
+    const header = lang === "zulu" ? "Funda Amagama!" : "Learn Words!";
+
+    return html`
+        <header class="main-header">
+            <img class="logo" src=${CdnPath.common("tj-logo-header.png")} />
+            <div class="text">${header}</div>
+        </header>
+    `;
+}
