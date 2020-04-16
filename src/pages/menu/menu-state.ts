d5c78134a2abc2360de4137a43591a04852800c1
David Komer <david.komer@gmail.com>
2020-04-16 12:28:35 +0300

    menu levels

--- src/pages/menu/menu-state.ts
+++ src/pages/menu/menu-state.ts
@@ -3,6 +3,7 @@ import * as F from "fluture";
 import {future_to_callback, invoke_future} from "@utils/xstate";
 import {fetchJsonUrl} from "fluture-loaders";
 import {CdnPath} from "@utils/path";
+import {router_service} from "@components/router/router-state";
 
 const machine = Machine({
     id: "menu",
@@ -14,7 +15,10 @@ const machine = Machine({
     states: {
         loading: { 
             invoke: {
-                src: invoke_future(loadMenuConfig) 
+                src: invoke_future(() => {
+                    const {level} = router_service.state.context;
+                    return loadMenuConfig(level);
+                }) 
             },
             on: {
                 RESOLVE: {
@@ -45,13 +49,14 @@ const machine = Machine({
     }
 });
 
-function loadMenuConfig() {
-    return fetchJsonUrl (CdnPath.root("config/menu.json"))
+function loadMenuConfig(level:string) {
+    return fetchJsonUrl (CdnPath.root(`config/menu-${level}.json`))
 }
 
 let _service;
 
 export const get_service = () => {
+
     if(!_service) {
         _service = interpret(machine).start();
     }
