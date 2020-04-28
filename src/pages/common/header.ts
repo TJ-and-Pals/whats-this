094debaadbabc5ee61f5e22c94b100e2f862391b
David Komer <david.komer@gmail.com>
2020-04-28 22:00:44 +0300

    logo should click to home

--- src/pages/common/header.ts
+++ src/pages/common/header.ts
@@ -2,14 +2,17 @@ import {html} from "lit-html";
 import "./header.css"
 import { CdnPath } from "@utils/path";
 import {set_language, get_language} from "@components/language-selector/language-selector-state";
+import {router_service} from "@components/router/router-state";
 export const header = () => {
     const lang = get_language();
 
     const header = lang === "zulu" ? "Funda Amagama!" : "Learn Words!";
 
+    const on_home = () => router_service.send("HOME");
+
     return html`
         <header class="main-header">
-            <img class="logo" src=${CdnPath.common("tj-logo-header.png")} />
+            <img class="logo" src=${CdnPath.common("tj-logo-header.png")} @click=${on_home} />
             <div class="text ${lang}">${header}</div>
         </header>
     `;
